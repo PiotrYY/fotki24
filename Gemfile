8d2b403dc9bdedd3b90d2a1c27cc6a5b113805b6
Piotr Juraszek <yuraszek@gmail.com>
2016-08-23 12:59:30 +0200

    paperclip, fix crate image

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'paperclip', '~> 4.3.0'
-gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
+gem 'paperclip' 
+gem 'aws-sdk'
+gem 'bootstrap-sass'
 gem 'devise'
 gem "font-awesome-rails"
 
--- app/controllers/fotos_controller.rb
+++ app/controllers/fotos_controller.rb
@@ -2,32 +2,23 @@ class FotosController < ApplicationController
   before_action :set_foto, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  # GET /fotos
-  # GET /fotos.json
+
   def index
     @fotos = Foto.all
   end
 
-  # GET /fotos/1
-  # GET /fotos/1.json
   def show
   end
 
-  # GET /fotos/new
   def new
     @foto = current_user.fotos.build
   end
 
-  # GET /fotos/1/edit
   def edit
   end
 
-  # POST /fotos
-  # POST /fotos.json
   def create
     @foto = current_user.fotos.build(foto_params)
-
-
     respond_to do |format|
       if @foto.save
         format.html { redirect_to @foto, notice: 'Foto was successfully created.' }
@@ -38,7 +29,6 @@ class FotosController < ApplicationController
       end
     end
   end
-
   # PATCH/PUT /fotos/1
   # PATCH/PUT /fotos/1.json
   def update
@@ -52,9 +42,6 @@ class FotosController < ApplicationController
       end
     end
   end
-
-  # DELETE /fotos/1
-  # DELETE /fotos/1.json
   def destroy
     @foto.destroy
     respond_to do |format|
@@ -63,20 +50,18 @@ class FotosController < ApplicationController
     end
   end
 
+
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_foto
       @foto = Foto.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def foto_params
       params.require(:foto).permit(:description, :image)
-    # def correct_user
+    end
+
     def correct_user
       @foto = current_user.fotos.find_by(id: params[:id])
-      redirect_to fotos_path, notice: "Nie jesteś uprawniony
-      do edycji tego zdjęcia" if @photo.nil?
+      redirect_to fotos_path, notice: "Nie jesteś uprawniony do edycji tego zdjęcia" if @photo.nil?
       end
-    end
 end
